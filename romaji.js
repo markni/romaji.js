92fa195b623662cd30e115989ed6ef3b084642ff
Mark Ni <xixian.ni@gmail.com>
2013-11-22 12:17:59 -0800

    added very basic support for toHiragana() function. still need work on. updated test cases for it.

--- romaji.js
+++ romaji.js
@@ -14,8 +14,403 @@
         romaji = root.romaji = {};
     }
 
-    romaji.version = '0.2.0';
-    romaji.model = 'hepburn';
+    romaji.version = '0.2.1';
+    romaji.mode = 'hepburn';
+
+
+
+
+
+
+
+
+    //helper functions
+
+    var swapJsonKeyValues = function (input) {
+        var one, output = {};
+        for (one in input) {
+            if (input.hasOwnProperty(one)) {
+                output[input[one]] = one;
+            }
+        }
+        return output;
+    }
+
+
+    var replaceAll = function (find, replace, str) {
+        return str.replace(new RegExp(find, 'g'), replace);
+    }
+
+
+
+    //maps
+    var toKanaMap = {};
+
+    toKanaMap.hiragana = {
+        //Digraphs
+
+        "きゃ": "kya",
+        "しゃ": "sha",
+        "ちゃ": "cha",
+        "にゃ": "nya",
+        "ひゃ": "hya",
+        "みゃ": "mya",
+        "りゃ": "rya",
+        "ぎゃ": "gya",
+        "ふゅ": "fyu",
+
+        "びゃ": "bya",
+        "ぴゃ": "pya",
+
+        "きゅ": "kyu",
+        "しゅ": "shu",
+        "ちゅ": "chu",
+        "にゅ": "nyu",
+        "ひゅ": "hyu",
+        "みゅ": "myu",
+        "りゅ": "ryu",
+        "ぎゅ": "gyu",
+
+        "びゅ": "byu",
+        "ぴゅ": "pyu",
+
+        "きょ": "kyo",
+        "しょ": "sho",
+        "ちょ": "cho",
+        "にょ": "nyo",
+        "ひょ": "hyo",
+        "みょ": "myo",
+        "りょ": "ryo",
+        "ぎょ": "gyo",
+
+        "びょ": "byo",
+        "ぴょ": "pyo",
+
+
+
+        "つぁ": "tsa",
+        "つぃ": "tsi",
+        "つぇ": "tse",
+        "つぉ": "tso",
+
+        "ちぇ": "che",
+        "しぇ": "she",
+
+
+        "じゃ": "ja",
+        "じゅ": "ju",
+        "じょ": "jo",
+
+        "ふぁ": "fa",
+        "ふぃ": "fi",
+        "ふぇ": "fe",
+        "ふぉ": "fo",
+
+
+        "うぃ": "wi",
+        "うぇ": "we",
+        "うぉ": "wo",
+
+        "ゔぁ": "va",
+        "ゔぃ": "vi",
+        "ゔぇ": "ve",
+        "ゔぉ": "vo",
+
+        "じぇ": "je",
+        "てぃ": "ti",
+        "でぃ": "di",
+        "でゅ": "du",
+        "とぅ": "tu",
+
+        //Monographs
+
+        "し": "shi",
+        "ち": "chi",
+        "つ": "tsu",
+
+
+        "か": "ka",
+        "さ": "sa",
+        "た": "ta",
+        "な": "na",
+        "は": "ha",
+        "ま": "ma",
+        "ら": "ra",
+
+        "き": "ki",
+
+        "に": "ni",
+        "ひ": "hi",
+        "み": "mi",
+        "り": "ri",
+
+        "く": "ku",
+
+        "す": "su",
+
+        "ぬ": "nu",
+        "ふ": "fu",
+        "む": "mu",
+        "る": "ru",
+
+        "け": "ke",
+        "せ": "se",
+        "て": "te",
+        "ね": "ne",
+        "へ": "he",
+        "め": "me",
+        "れ": "re",
+
+        "こ": "ko",
+        "そ": "so",
+        "と": "to",
+        "の": "no",
+        "ほ": "ho",
+        "も": "mo",
+        "ろ": "ro",
+
+        "わ": "wa",
+        "を": "wo",
+
+
+
+        "が": "ga",
+        "ざ": "za",
+        "だ": "da",
+        "ば": "ba",
+        "ぱ": "pa",
+
+        "ぎ": "gi",
+
+
+        "ぢ": "ji",
+        //じ is more common to use, so it goes secondly
+        // when we build toHiraganaMap, this will get overwriiten by second one
+
+        "じ": "ji",
+
+
+
+        "び": "bi",
+        "ぴ": "pi",
+
+        "ぐ": "gu",
+        "ず": "zu",
+        "づ": "zu",
+        "ぶ": "bu",
+        "ぷ": "pu",
+
+        "げ": "ge",
+        "ぜ": "ze",
+        "で": "de",
+        "べ": "be",
+        "ぺ": "pe",
+
+        "ご": "go",
+        "ぞ": "zo",
+        "ど": "do",
+        "ぼ": "bo",
+        "ぽ": "po",
+
+        "や": "ya",
+        "ゆ": "yu",
+        "よ": "yo",
+
+
+        // obsoleted kana
+
+        "ゑ": "we",
+
+
+
+        "あ": "a",
+        "い": "i",
+        "う": "u",
+        "え": "e",
+        "お": "o",
+        "ん": "n"
+
+
+    }
+
+    toKanaMap.katakana = {
+        "キャ": "kya",
+        "シャ": "sha",
+        "チャ": "cha",
+        "ニャ": "nya",
+        "ヒャ": "hya",
+        "ミャ": "mya",
+        "リャ": "rya",
+        "ギャ": "gya",
+        "ジャ": "ja",
+        "ビャ": "bya",
+        "ピャ": "pya",
+
+        "キュ": "kyu",
+        "シュ": "shu",
+        "チュ": "chu",
+        "ニュ": "nyu",
+        "ヒュ": "hyu",
+        "ミュ": "myu",
+        "リュ": "ryu",
+        "ギュ": "gyu",
+        "ジュ": "ju",
+        "ビュ": "byu",
+        "ピュ": "pyu",
+
+        "キョ": "kyo",
+        "ショ": "sho",
+        "チョ": "cho",
+        "ニョ": "nyo",
+        "ヒョ": "hyo",
+        "ミョ": "myo",
+        "リョ": "ryo",
+        "ギョ": "gyo",
+        "ジョ": "jo",
+        "ビョ": "byo",
+        "ピョ": "pyo",
+
+        "ファ": "fa",
+        "フィ": "fi",
+        "フェ": "fe",
+        "フォ": "fo",
+        "フュ": "fyu",
+
+        "ウィ": "wi",
+        "ウェ": "we",
+        "ウォ": "wo",
+
+        "ヴァ": "va",
+        "ヴィ": "vi",
+        "ヴェ": "ve",
+        "ヴォ": "vo",
+
+        "ツァ": "tsa",
+        "ツィ": "tsi",
+        "ツェ": "tse",
+        "ツォ": "tso",
+
+        "チェ": "che",
+        "シェ": "she",
+        "ジェ": "je",
+        "ティ": "ti",
+        "ディ": "di",
+        "デュ": "du",
+        "トゥ": "tu",
+
+
+        //basic
+
+        "ア": "a",
+        "カ": "ka",
+        "サ": "sa",
+        "タ": "ta",
+        "ナ": "na",
+        "ハ": "ha",
+        "マ": "ma",
+        "ラ": "ra",
+
+        "イ": "i",
+        "キ": "ki",
+        "シ": "shi",
+        "チ": "chi",
+        "ニ": "ni",
+        "ヒ": "hi",
+        "ミ": "mi",
+        "リ": "ri",
+
+        "ウ": "u",
+        "ク": "ku",
+        "ス": "su",
+        "ツ": "tsu",
+        "ヌ": "nu",
+        "フ": "fu",
+        "ム": "mu",
+        "ル": "ru",
+
+        "エ": "e",
+        "ケ": "ke",
+        "セ": "se",
+        "テ": "te",
+        "ネ": "ne",
+        "ヘ": "he",
+        "メ": "me",
+        "レ": "re",
+
+        "オ": "o",
+        "コ": "ko",
+        "ソ": "so",
+        "ト": "to",
+        "ノ": "no",
+        "ホ": "ho",
+        "モ": "mo",
+        "ロ": "ro",
+
+        "ワ": "wa",
+        "ヲ": "wo",
+        "ン": "n",
+
+
+        "ガ": "ga",
+        "ザ": "za",
+        "ダ": "da",
+        "バ": "ba",
+        "パ": "pa",
+
+        "ギ": "gi",
+        "ジ": "ji",
+        "ヂ": "ji",
+        "ビ": "bi",
+        "ピ": "pi",
+
+        "グ": "gu",
+        "ズ": "zu",
+        "ヅ": "zu",
+        "ブ": "bu",
+        "プ": "pu",
+
+        "ゲ": "ge",
+        "ぜ": "ze",
+        "デ": "de",
+        "ベ": "be",
+        "ペ": "pe",
+
+        "ゴ": "go",
+        "ゾ": "zo",
+        "ド": "do",
+        "ボ": "bo",
+        "ポ": "po",
+
+        "ャ": "ya", "ヤ": "ya",
+        "ュ": "yu", "ユ": "yu",
+        "ョ": "yo", "ヨ": "yo",
+
+
+        // obsoleted kana
+
+        "ヱ": "we",
+        "ヹ": "ve"
+
+
+    }
+
+
+    var longVowelsMap = {
+        'a': 'ā',
+        'e': 'ē',
+        'i': 'ī',
+        'o': 'ō',
+        'u': 'ū'
+    }
+
+
+    //rebuild map by switching key and value
+    var toHiraganaMap = swapJsonKeyValues(toKanaMap.hiragana);
+    var toKatakanaMap = swapJsonKeyValues(toKanaMap.katakana);
+
+
+
+
 
     romaji.noConflict = function () {
         root.romaji = previousRomaji;
@@ -23,374 +418,55 @@
     }
 
 
-    romaji.fromKana = function (syllabary) {
-
-        var result = syllabary;
-
-        var dics = {};
-
-        dics.hiragana = {
-            //combined first
-            "きゃ": "kya",
-            "しゃ": "sha",
-            "ちゃ": "cha",
-            "にゃ": "nya",
-            "ひゃ": "hya",
-            "みゃ": "mya",
-            "りゃ": "rya",
-            "ぎゃ": "gya",
-            "じゃ": "ja",
-            "びゃ": "bya",
-            "ぴゃ": "pya",
-
-            "きゅ": "kyu",
-            "しゅ": "shu",
-            "ちゅ": "chu",
-            "にゅ": "nyu",
-            "ひゅ": "hyu",
-            "みゅ": "myu",
-            "りゅ": "ryu",
-            "ぎゅ": "gyu",
-            "じゅ": "ju",
-            "びゅ": "byu",
-            "ぴゅ": "pyu",
-
-            "きょ": "kyo",
-            "しょ": "sho",
-            "ちょ": "cho",
-            "にょ": "nyo",
-            "ひょ": "hyo",
-            "みょ": "myo",
-            "りょ": "ryo",
-            "ぎょ": "gyo",
-            "じょ": "jo",
-            "びょ": "byo",
-            "ぴょ": "pyo",
+    //main functions
 
 
-            "ふぁ": "fa",
-            "ふぃ": "fi",
-            "ふぇ": "fe",
-            "ふぉ": "fo",
-            "ふゅ": "fyu",
-
-            "うぃ": "wi",
-            "うぇ": "we",
-            "うぉ": "wo",
-
-            "ゔぁ": "va",
-            "ゔぃ": "vi",
-            "ゔぇ": "ve",
-            "ゔぉ": "vo",
-
-            "つぁ": "tsa",
-            "つぃ": "tsi",
-            "つぇ": "tse",
-            "つぉ": "tso",
-
-            "ちぇ": "che",
-            "しぇ": "she",
-            "じぇ": "je",
-            "てぃ": "ti",
-            "でぃ": "di",
-            "でゅ": "du",
-            "とぅ": "tu",
-
-            //basic
-
-            "あ": "a",
-            "か": "ka",
-            "さ": "sa",
-            "た": "ta",
-            "な": "na",
-            "は": "ha",
-            "ま": "ma",
-            "ら": "ra",
-
-            "い": "i",
-            "き": "ki",
-            "し": "shi",
-            "ち": "chi",
-            "に": "ni",
-            "ひ": "hi",
-            "み": "mi",
-            "り": "ri",
-
-            "う": "u",
-            "く": "ku",
-            "す": "su",
-            "つ": "tsu",
-            "ぬ": "nu",
-            "ふ": "fu",
-            "む": "mu",
-            "る": "ru",
-
-            "え": "e",
-            "け": "ke",
-            "せ": "se",
-            "て": "te",
-            "ね": "ne",
-            "へ": "he",
-            "め": "me",
-            "れ": "re",
-
-            "お": "o",
-            "こ": "ko",
-            "そ": "so",
-            "と": "to",
-            "の": "no",
-            "ほ": "ho",
-            "も": "mo",
-            "ろ": "ro",
-
-            "わ": "wa",
-            "を": "wo",
-            "ん": "n",
-
-
-            "が": "ga",
-            "ざ": "za",
-            "だ": "da",
-            "ば": "ba",
-            "ぱ": "pa",
-
-            "ぎ": "gi",
-            "じ": "ji",
-            "ぢ": "ji",
-            "び": "bi",
-            "ぴ": "pi",
-
-            "ぐ": "gu",
-            "ず": "zu",
-            "づ": "zu",
-            "ぶ": "bu",
-            "ぷ": "pu",
-
-            "げ": "ge",
-            "ぜ": "ze",
-            "で": "de",
-            "べ": "be",
-            "ぺ": "pe",
-
-            "ご": "go",
-            "ぞ": "zo",
-            "ど": "do",
-            "ぼ": "bo",
-            "ぽ": "po",
-
-            "や": "ya",
-            "ゆ": "yu",
-            "よ": "yo",
-
-
-            // obsoleted kana
-
-            "ゑ":"we"
-
-
+    romaji.toHiragana = function (romaji) {
+        var result = romaji;
+        for (var r in toHiraganaMap) {
+            result = replaceAll(r, toHiraganaMap[r], result);
         }
 
-        dics.katakana = {
-            "キャ": "kya",
-            "シャ": "sha",
-            "チャ": "cha",
-            "ニャ": "nya",
-            "ヒャ": "hya",
-            "ミャ": "mya",
-            "リャ": "rya",
-            "ギャ": "gya",
-            "ジャ": "ja",
-            "ビャ": "bya",
-            "ピャ": "pya",
-
-            "キュ": "kyu",
-            "シュ": "shu",
-            "チュ": "chu",
-            "ニュ": "nyu",
-            "ヒュ": "hyu",
-            "ミュ": "myu",
-            "リュ": "ryu",
-            "ギュ": "gyu",
-            "ジュ": "ju",
-            "ビュ": "byu",
-            "ピュ": "pyu",
-
-            "キョ": "kyo",
-            "ショ": "sho",
-            "チョ": "cho",
-            "ニョ": "nyo",
-            "ヒョ": "hyo",
-            "ミョ": "myo",
-            "リョ": "ryo",
-            "ギョ": "gyo",
-            "ジョ": "jo",
-            "ビョ": "byo",
-            "ピョ": "pyo",
-
-            "ファ": "fa",
-            "フィ": "fi",
-            "フェ": "fe",
-            "フォ": "fo",
-            "フュ": "fyu",
-
-            "ウィ": "wi",
-            "ウェ": "we",
-            "ウォ": "wo",
-
-            "ヴァ": "va",
-            "ヴィ": "vi",
-            "ヴェ": "ve",
-            "ヴォ": "vo",
-
-            "ツァ": "tsa",
-            "ツィ": "tsi",
-            "ツェ": "tse",
-            "ツォ": "tso",
-
-            "チェ": "che",
-            "シェ": "she",
-            "ジェ": "je",
-            "ティ": "ti",
-            "ディ": "di",
-            "デュ": "du",
-            "トゥ": "tu",
+        return result;
 
 
-            //basic
+    };
 
-            "ア": "a",
-            "カ": "ka",
-            "サ": "sa",
-            "タ": "ta",
-            "ナ": "na",
-            "ハ": "ha",
-            "マ": "ma",
-            "ラ": "ra",
+    romaji.toKatakana = function () {
+        console.log('!!');
 
-            "イ": "i",
-            "キ": "ki",
-            "シ": "shi",
-            "チ": "chi",
-            "ニ": "ni",
-            "ヒ": "hi",
-            "ミ": "mi",
-            "リ": "ri",
+    };
 
-            "ウ": "u",
-            "ク": "ku",
-            "ス": "su",
-            "ツ": "tsu",
-            "ヌ": "nu",
-            "フ": "fu",
-            "ム": "mu",
-            "ル": "ru",
+    romaji.fromKana = function (kana) {
 
-            "エ": "e",
-            "ケ": "ke",
-            "セ": "se",
-            "テ": "te",
-            "ネ": "ne",
-            "ヘ": "he",
-            "メ": "me",
-            "レ": "re",
-
-            "オ": "o",
-            "コ": "ko",
-            "ソ": "so",
-            "ト": "to",
-            "ノ": "no",
-            "ホ": "ho",
-            "モ": "mo",
-            "ロ": "ro",
-
-            "ワ": "wa",
-            "ヲ": "wo",
-            "ン": "n",
-
-
-            "ガ": "ga",
-            "ザ": "za",
-            "ダ": "da",
-            "バ": "ba",
-            "パ": "pa",
-
-            "ギ": "gi",
-            "ジ": "ji",
-            "ヂ": "ji",
-            "ビ": "bi",
-            "ピ": "pi",
-
-            "グ": "gu",
-            "ズ": "zu",
-            "ヅ": "zu",
-            "ブ": "bu",
-            "プ": "pu",
-
-            "ゲ": "ge",
-            "ぜ": "ze",
-            "デ": "de",
-            "ベ": "be",
-            "ペ": "pe",
-
-            "ゴ": "go",
-            "ゾ": "zo",
-            "ド": "do",
-            "ボ": "bo",
-            "ポ": "po",
-
-            "ャ": "ya", "ヤ": "ya",
-            "ュ": "yu", "ユ": "yu",
-            "ョ": "yo", "ヨ": "yo",
-
-
-            // obsoleted kana
-
-            "ヱ":"we",
-            "ヹ":"ve"
-
-
-        }
-
-        var longvowels = {
-            'a': 'ā',
-            'e': 'ē',
-            'i': 'ī',
-            'o': 'ō',
-            'u': 'ū'
-        }
-
-
-        var replaceAll = function (find, replace, str) {
-            return str.replace(new RegExp(find, 'g'), replace);
-        }
+        var result = kana;
 
         var longVowelsReplacer = function (match, p1) {
-            return longvowels[p1];
+            return longVowelsMap[p1];
         }
 
-
-        for (var index in dics) {
-            for (var s in dics[index]) {
-                result = replaceAll(s, dics[index][s], result);
+        //basic dictionary matching
+        for (var index in toKanaMap) {
+            for (var s in toKanaMap[index]) {
+                result = replaceAll(s, toKanaMap[index][s], result);
             }
         }
 
         //replace long vowels
-        result = result.replace(/([aeiou])(ー)/g, longVowelsReplacer);
+        result = result.replace(/([aeiou])ー/g, longVowelsReplacer);
 
         //replace the sokuon (doubling)
         result = result.replace(/(ッ|っ)([a-z])/g, "$2$2");
-
 
         return result;
 
     }
 
-    romaji.convert = function(s){
+    romaji.convert = function (s) {
         console.warn('WARNING: romaji.convert() is deprecated, please use romjia.fromKana()');
         return romaji.fromKana(s);
-
     }
+
 
 
 }).call(this);
--- test/test.romaji.js
+++ test/test.romaji.js
@@ -2,39 +2,51 @@ var assert = require("assert");
 var romaji = require("../romaji.js");
 
 
-describe('romaji.js', function(){
+describe('romaji.js', function () {
 
-    describe('#noConflict()', function(){
-        it('should return romaji object',function(){
+    describe('#noConflict()', function () {
+        it('should return romaji object', function () {
             var noconflictromaji = romaji.noConflict();
             assert.equal(romaji, noconflictromaji);
         })
     })
 
-    describe('#fromKana()', function(){
-        it('should return the correct rōmaji from hiragana', function(){
-            assert.equal(romaji.fromKana('ふじさん'),'fujisan');
-            assert.equal(romaji.fromKana('おちゃ'),'ocha');
-            assert.equal(romaji.fromKana('ちじ'),'chiji');
-            assert.equal(romaji.fromKana('ちぢむ'),'chijimu');
-            assert.equal(romaji.fromKana('つづく'),'tsuzuku');
+    describe('#fromKana()', function () {
+        it('should return the correct rōmaji from hiragana', function () {
+            assert.equal(romaji.fromKana('ふじさん'), 'fujisan');
+            assert.equal(romaji.fromKana('おちゃ'), 'ocha');
+            assert.equal(romaji.fromKana('ちじ'), 'chiji');
+            assert.equal(romaji.fromKana('ちぢむ'), 'chijimu');
+            assert.equal(romaji.fromKana('つづく'), 'tsuzuku');
 
         })
-        it('should return the correct rōmaji from katakana', function(){
-            assert.equal(romaji.fromKana('フネカヂモガモ'),'funekajimogamo');
+        it('should return the correct rōmaji from katakana', function () {
+            assert.equal(romaji.fromKana('フネカヂモガモ'), 'funekajimogamo');
 
 
         })
-        it('should return the correct long vowels conversion from katakana', function(){
-            assert.equal(romaji.fromKana('チャーハン'),'chāhan');
-            assert.equal(romaji.fromKana('ローマ'),'rōma');
-            assert.equal(romaji.fromKana('いムー'),'imū');
+        it('should return the correct long vowels conversion from katakana', function () {
+            assert.equal(romaji.fromKana('チャーハン'), 'chāhan');
+            assert.equal(romaji.fromKana('ローマ'), 'rōma');
+            assert.equal(romaji.fromKana('いムー'), 'imū');
 
         })
-        it('should return the correct doubling for sokuon', function(){
-            assert.equal(romaji.fromKana('モット'),'motto');
+        it('should return the correct doubling for sokuon', function () {
+            assert.equal(romaji.fromKana('モット'), 'motto');
         })
     })
+
+
+
+    describe('#toHiragana()', function () {
+        it('should return the correct hiragana from rōmaji', function () {
+            assert.equal(romaji.toHiragana('fujisan'), 'ふじさん');
+            assert.equal(romaji.toHiragana('ocha'),'おちゃ');
+            assert.equal(romaji.toHiragana('chiji'),'ちじ');
+            assert.equal(romaji.toHiragana('tsuzuku'),'つづく');
+        })
+    })
+
 
 })
 
